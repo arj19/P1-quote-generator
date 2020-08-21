21237f6da1d99bafe309beb9a569ac78443ea453
arjun <phani.stark@outlook.com>
2020-08-21 13:53:18 +0530

    updated js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+async function getQuote(){
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try{
+        const reponse = await fetch(apiUrl);
+        const data = await response.json();
+        document.getElementById("quote").innerText = data.quoteText;
+        console.log(data);
+    }catch(error){
+        getQuote();
+        console.log(error);
+    }
+
+}
